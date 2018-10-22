a2fb9d754135de1be8a6ef30c3a9c54494dcc346
Alice Mahieu <alicemahieu@tsf-452-wpa-4-069.epfl.ch>
2018-10-22 14:21:26 +0200

    Initial commit

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/alicemahieu/Desktop/BA3/Projetinfo/Exercice
+# Build directory: /Users/alicemahieu/Desktop/BA3/Projetinfo/Exercice/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Network_simulation "testNetSim")
---
+++ src/network.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alice Mahieu on 22/10/2018.
+//
+
+#include "network.h"
